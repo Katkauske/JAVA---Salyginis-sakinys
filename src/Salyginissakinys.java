2bdf7ef8dcbdc79d2d61ce6f8b127ff75f191652
Katkauske <kkatkauske@gmail.com>
2022-11-09 09:29:42 +0200

    Uzduotys

--- src/Salyginissakinys.java
+++ src/Salyginissakinys.java
@@ -1,11 +1,23 @@
 public class Salyginissakinys {
     public static void main(String[] args) {
-        int skaicius = 21;
-        System.out.println(arLyginis(skaicius));
+        int skaicius = 1;
+        int skaicius1 = 5;
+        System.out.println("Ar lyginis:" + arLyginis(skaicius));
+        System.out.println("Ar neigiamas:" + arNeigiamas(skaicius));
+        System.out.println("Didesne sandauga ar suma:" + sandauga(skaicius, skaicius1));
     }
         public static boolean arLyginis(int skaicius){
             return skaicius % 2 == 0;
 
     }
+
+        public static boolean arNeigiamas(int skaicius){
+        return skaicius > 0;
+        }
+
+
+        public static boolean sandauga(double skaicius, double skaicius1){
+        return ( skaicius * skaicius) > (skaicius + skaicius);
+
+        }
 }
-//1. Sukurkite funkciją, kuri gauna int skaičių ir patikrina ar jis yra lyginis. Grąžina boolean.
