2dda17a66d9b12d32a72908e8a96b8bfd59adce2
Katkauske <kkatkauske@gmail.com>
2022-11-09 09:18:44 +0200

    Salyginio sakinio uzduotys

---
+++ src/Salyginissakinys.java
@@ -0,0 +1,11 @@
+public class Salyginissakinys {
+    public static void main(String[] args) {
+        int skaicius = 21;
+        System.out.println(arLyginis(skaicius));
+    }
+        public static boolean arLyginis(int skaicius){
+            return skaicius % 2 == 0;
+
+    }
+}
+//1. Sukurkite funkciją, kuri gauna int skaičių ir patikrina ar jis yra lyginis. Grąžina boolean.
